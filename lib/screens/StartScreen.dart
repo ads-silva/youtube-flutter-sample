5469faea23c150715b08f543b3b340986ed39a3a
Adauto de Souza Silva <dautodroid@gmail.com>
2021-06-03 13:31:19 -0300

    feat: add start screen file

---
+++ lib/screens/StartScreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatefulWidget {
+  const StartScreen({Key key}) : super(key: key);
+
+  @override
+  _StartScreenState createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        "Inicío",
+        style: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );return Container();
+  }
+}
