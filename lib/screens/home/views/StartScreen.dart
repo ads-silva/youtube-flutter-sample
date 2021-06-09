a7eb35f80ac87ffe6568e052a52537e429d6f1a5
Adauto de Souza Silva <dautodroid@gmail.com>
2021-06-08 23:02:55 -0300

    feat: add api youtube service

--- lib/screens/home/views/StartScreen.dart
+++ lib/screens/home/views/StartScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_flutter_sample/services/ApiYoutube.dart';
 
 class StartScreen extends StatefulWidget {
   const StartScreen({Key key}) : super(key: key);
@@ -10,6 +11,10 @@ class StartScreen extends StatefulWidget {
 class _StartScreenState extends State<StartScreen> {
   @override
   Widget build(BuildContext context) {
+
+    ApiYoutube apiYoutube = ApiYoutube();
+    apiYoutube.search("");
+
     return Container(
       child: Text(
         "Inicío",
