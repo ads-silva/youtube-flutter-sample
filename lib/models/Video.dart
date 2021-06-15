dd621b69c7fa28f5527a607c7559dd0e91fff820
Adauto de Souza Silva <dautodroid@gmail.com>
2021-06-14 21:41:30 -0300

    feat: add parseListJson and factory constructor

--- lib/models/Video.dart
+++ lib/models/Video.dart
@@ -7,4 +7,18 @@ class Video {
   String chanel;
 
   Video({this.id, this.title, this.description, this.image, this.chanel});
+
+  static List<Video> parseListJson(Map<String, dynamic> data, key) {
+    return data[key].map<Video>((item) => Video.fromJson(item)).toList();
+  }
+
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      id: json["id"]["videoId"],
+      title: json["snippet"]["title"],
+      // description: json["description"],
+      image: json["snippet"]["thumbnails"]["high"]["url"],
+      chanel: json["snippet"]["channelId"],
+    );
+  }
 }
